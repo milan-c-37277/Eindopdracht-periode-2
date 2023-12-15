1f6100e7879b16ac865b295c062f3997bab6343d
Milan Carati <37277@ma-web.nl>
2023-12-15 10:37:32 +0100

    Push

--- js/app.js
+++ js/app.js
@@ -1,10 +1,9 @@
-
 let checkbox = document.getElementById("taalswitch");
 checkbox.addEventListener("click",(e)=>
 {
-    console.log(e.target.checked);
     if(e.target.checked == true) {
         window.location.href ="./engels.html";
+        
 
     } else {
         window.location.href ="./index.html";

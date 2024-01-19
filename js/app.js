c5e9925a2b46a38058d56541873e3e4ea5234f30
Coo1boy2169 <>
2024-01-19 10:53:19 +0100

    Web Site Meer Toegevoegt

--- js/app.js
+++ js/app.js
@@ -74,3 +74,41 @@ function myFunction() {
       moreText.style.display = "inline";
     }
   }
+
+  function myFunction2() {
+    var dots2 = document.getElementById("dots2");
+    var moreText2 = document.getElementById("more2");
+    var btnText2 = document.getElementById("myBtn2");
+  
+    if (dots2.style.display === "none") {
+      dots2.style.display = "inline";
+      btnText2.innerHTML = "Meer";
+      moreText2.style.display = "none";
+    } else {
+      dots2.style.display = "none";
+      btnText2.innerHTML = "Minder";
+      moreText2.style.display = "inline";
+    }
+  }
+
+
+  function myFunction3() {
+    var dots = document.getElementById("dots3");
+    var moreText = document.getElementById("more3");
+    var btnText = document.getElementById("myBtn3");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Meer";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Minder";
+      moreText.style.display = "inline";
+    }
+  }
+
+
+
+
+  
